9a27f0186dfa45237bdb1e8be12c0fafe32bf6a2
Ashwin Rajesh <ashwinr2k2@gmail.com>
2022-07-22 23:37:52 +1000

    Home Screen

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{alignItems: 'center'}}>
+      <h1 style={{padding: 20}}>Movie Ticket App</h1>
+      <Button 
+        title="Book Tickets"
+        onPress={() => navigation.navigate('Movies')}
+      />
+    </View>
+  );
+}
+
+function MovieSelect() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
+
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Movies" component={MovieSelect} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -16,5 +45,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
   },
 });
